e168a87868bc8a5f871f315e2d5eca4e73f62596
sachinchouksey017 <sachinchouksey017@gmail.com>
2021-05-25 19:21:05 +0530

    Added header and welcome box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppMaterial } from './app.material.module';
+import { HeaderComponent } from './components/header/header.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { DisplayComponent } from './components/display/display.component';
+import { HomeComponent } from './components/home/home.component';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WelcomeComponent,
+    DisplayComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppMaterial,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
